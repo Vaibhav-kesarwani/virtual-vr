5af60e5b3a19f8e4167ec4da7cc2c97997107057
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-08-24 19:28:39 +0530

    Basic Hello World

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import './App.css'
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
-
-export default App
+export default App;
