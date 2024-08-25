ddf0c6868aa7e19054047061b016894694a5ff01
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-08-25 15:42:27 +0530

    Basic nav complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
-            <div className="flex justify-center items-center">
+            <div className="flex justify-between items-center">
                 <div className="flex items-center flex-shrink-0">
                     <img src={logo} alt="logo" className="h-10 w-10 mr-2" />
                     <span className="text-xl tracking-tight">Virtual VR</span>  
@@ -17,6 +17,14 @@ const Navbar = () => {
                         </li>
                     ))}
                 </ul>
+                <div className="hidden lg:flex justify-center space-x-12 items-center">
+                    <a href="#" className="py-2 px-3 border rounded-md">
+                        Sign In
+                    </a>
+                    <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md">
+                        Create an account
+                    </a>
+                </div>
             </div>
         </div>
     </nav>
