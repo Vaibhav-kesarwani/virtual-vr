ba26ea05431f937f5aa244ebfda03c294866ac57
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-08-25 15:54:14 +0530

    Completed the Navbar & also make it responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
+import {Menu, X} from "lucide-react";
+import {useState} from "react";
 import logo from "../assets/logo.png";
 import {navItems} from "../constants";
 
 const Navbar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+    const toggleNavbar = () => setMobileDrawerOpen(!mobileDrawerOpen);
+
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
@@ -25,7 +30,31 @@ const Navbar = () => {
                         Create an account
                     </a>
                 </div>
+                <div className="lg:hidden md:flex flex-col justify-end">
+                    <button onClick={toggleNavbar}>
+                        {mobileDrawerOpen ? <X size={24} /> : <Menu size={24} />}
+                    </button>
+                </div>
             </div>
+            {mobileDrawerOpen && (
+                <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center ld:hidden">
+                    <ul>
+                        {navItems.map((item, index) => (
+                            <li key={index} className="py-4">
+                                <a href={item.href}>{item.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="flex space-x-6">
+                        <a href="#" className="py-2 px-3 border rounded-md">
+                            Sign In
+                        </a>
+                        <a href="#" className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800">
+                            Create an account
+                        </a>
+                    </div>
+                </div>
+            )}
         </div>
     </nav>
   )
