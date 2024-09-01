5b6d9e1031216aa3b0157da87ac156b30d371c64
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-01 11:57:36 +0530

    Completed the video section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -16,6 +16,37 @@ const HeroSection = () => {
         intuitive development tools. Get started today and turn your imagination
         into immersive experiences.
       </p>
+      <div className="flex justify-center my-10">
+        <a
+          href="#"
+          className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
+        >
+          Start for free
+        </a>
+        <a href="#" className="py-3 px-4 mx-3 rounded-md border">
+          Documentation
+        </a>
+      </div>
+      <div className="flex mt-10 justify-center">
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"
+        >
+          <source src={video1} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"
+        >
+          <source src={video2} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
 };
