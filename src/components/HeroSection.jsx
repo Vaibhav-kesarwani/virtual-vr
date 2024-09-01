ade57350987143b0baaa62686dd4cdf2d2617b9b
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-01 11:48:16 +0530

    Add the paragraph set

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,6 +11,11 @@ const HeroSection = () => {
           for developers
         </span>
       </h1>
+      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive experiences.
+      </p>
     </div>
   );
 };
