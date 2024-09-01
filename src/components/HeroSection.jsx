72ee135dc567317bf98cede88a08e37d4cd0e11a
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-01 11:44:24 +0530

    Make the text gradient

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,18 @@
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+
 const HeroSection = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        Virtual build tools
+        <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+          {" "}
+          for developers
+        </span>
+      </h1>
+    </div>
+  );
 };
 
 export default HeroSection;
